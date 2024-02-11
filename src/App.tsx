e79c05e8b7199353ad0f1db1941dcea51d837745
andbudha <andreibartov@gmail.com>
2024-02-11 21:55:19 +0100

    TodoAppContainer updated&styled

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import { TodoAppContainer } from './components/TodoAppContainer/TodoAppContainer';
 
 const styles = {
-  bg: `h-screen w-screen bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+  bg: `h-screen w-screen bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0] p-5`,
 };
 function App() {
   return (
--- src/components/TodoAppContainer/TodoAppContainer.tsx
+++ src/components/TodoAppContainer/TodoAppContainer.tsx
@@ -1,11 +1,14 @@
 import { TodoAppForm } from '../TodoAppForm/TodoAppForm';
 import { TodoAppList } from '../TodoAppList/TodoAppList';
-const styles = { bg: `` };
+
+const styles = {
+  box: `container w-96 mx-auto bg-[#e7e5e4] p-5 m-5 rounded-md`,
+};
 type Props = {};
 
 export const TodoAppContainer = (props: Props) => {
   return (
-    <div className={styles.bg}>
+    <div className={styles.box}>
       <h2>Task Manager</h2>
       <TodoAppForm />
       <TodoAppList />
