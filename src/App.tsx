5ed99b9ab1326f945ed25687658ecd065f71b162
andbudha <andreibartov@gmail.com>
2024-02-14 11:40:54 +0100

    url updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from 'react';
 import './App.css';
 import { TodoAppContainer } from './components/TodoAppContainer/TodoAppContainer';
+import { useAppDispatch } from './redux/store';
+import { tasksThunks } from './redux/todolistSlice';
 
 const styles = {
   bg: `h-screen w-screen bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0] p-5`,
 };
 function App() {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(tasksThunks.fetchTasks());
+  }, []);
   return (
     <div className={styles.bg}>
       <TodoAppContainer />
--- src/api/todolistapi.tsx
+++ src/api/todolistapi.tsx
@@ -3,9 +3,30 @@ import { TaskType } from '../redux/todolistSlice';
 
 export const todolistAPI = {
   getTasks: () => {
-    return axios.get('https://8dfcc811d3e9872d.mokky.dev/todos');
+    return axios.get<TaskType[]>(
+      'https://65cc8f82dd519126b83ee44f.mockapi.io/api/1/todos'
+    );
   },
   postTask: (arg: TaskType) => {
-    return axios.post('https://8dfcc811d3e9872d.mokky.dev/todos', { ...arg });
+    return axios.post(
+      'https://65cc8f82dd519126b83ee44f.mockapi.io/api/1/todos',
+      { ...arg }
+    );
   },
+  deleteTask: (taskID: number) => {
+    return axios.delete(
+      `https://65cc8f82dd519126b83ee44f.mockapi.io/api/1/todos/${taskID}`
+    );
+  },
+  updateTaskStatus: (arg: UpdateTaskStatusArg) => {
+    return axios.put<TaskType>(
+      `https://65cc8f82dd519126b83ee44f.mockapi.io/api/1/todos/${arg.taskID}`,
+      { isComplete: arg.value }
+    );
+  },
+};
+
+export type UpdateTaskStatusArg = {
+  value: boolean;
+  taskID: number;
 };
